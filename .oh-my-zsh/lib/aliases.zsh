f1176cd56c16e622a9364cddb13a0234db577ed7
Ptolemy Barnes <Ptolemy.Barnes@thetrainline.com>
2017-09-24 17:06:26 +0100

    Adds aliases

---
+++ .oh-my-zsh/lib/aliases.zsh
@@ -0,0 +1,4 @@
+# bonsai command
+alias bonsai="tree -I 'public|node_modules'"
+# config: git commands scoped to the home directory, for dotfiles.
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
