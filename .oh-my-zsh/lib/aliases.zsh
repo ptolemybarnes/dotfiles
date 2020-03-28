d26d6f27fd4834cc4f4c30e383ab307fc5979f61
Ptolemy Barnes <ptolemybarnes@gmail.com>
2020-03-28 15:10:51 +0100

    Removes .oh-my-zsh stuff

--- .oh-my-zsh/lib/aliases.zsh
+++
@@ -1,4 +0,0 @@
-# bonsai command
-alias bonsai="tree -I 'public|node_modules'"
-# config: git commands scoped to the home directory, for dotfiles.
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
