36cf1f9b6ce4addad49f329fa7df813301c64785
Ptolemy Barnes <ptolemybarnes@gmail.com>
2022-01-20 09:21:05 +0100

    updates snip function

--- .config/fish/functions/snip.fish
+++ .config/fish/functions/snip.fish
@@ -1,3 +1,3 @@
 function snip -d "Search code snippets"
-  ag -l (tail (fd --full-path '/Users/ptolemy/deck/code-snippets') | fzf --border --query="# " --preview='ag -l {} /Users/ptolemy/deck/code-snippets | xargs tail -n +2') /Users/ptolemy/deck/code-snippets | xargs tail -n +2 | fish
+  commandline (ag -l (tail (fd . '/Users/ptolemy/deck/code-snippets') | fzf --border --query="# " --preview='ag -l {} /Users/ptolemy/deck/code-snippets | xargs tail -n +2') /Users/ptolemy/deck/code-snippets | xargs tail -n +2)
 end
